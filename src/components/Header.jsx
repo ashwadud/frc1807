f05469278c8fed8f594f9b5104985344dc7df1b1
ashwud <wadud.a.2026@ufrsd.us>
2022-10-04 15:24:15 -0400

    push

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+const Header = ({title, image, children}) => {
+  return (
+    <header className="header">
+        <div className="header__container">
+            <div className="header__container-bg">
+                <img src={image} alt="Header Background Image" />
+            </div>
+            <div className="header__content">
+                <h2>{title}</h2>
+                <p>{children}</p>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,8 +1,14 @@
+import Header from '../../components/Header'
+import HeaderImage from '../../images/main__header.png'
 import './about.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <Header title="About Us" image={HeaderImage}>
+      Scroll down to read about our history and how we built our team from the start.
+    </Header>
+    </>
   )
 }
 
