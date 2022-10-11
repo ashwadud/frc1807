62a49a1e95bc310c5e31ba7ec7ce484238f64748
Aaryan <lightningd56@gmail.com>
2022-10-10 21:50:17 -0400

    Commit

--- src/pages/calendar/Calendar.jsx
+++ src/pages/calendar/Calendar.jsx
@@ -2,7 +2,9 @@ import './calendar.css'
 
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <div>
+      <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=MTgwN2ZyY0BnbWFpbC5jb20&color=%23EF6C00" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    </div>
   )
 }
 
